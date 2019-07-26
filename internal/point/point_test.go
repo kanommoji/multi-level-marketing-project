8aa717ee6df076df71202c9d790c67287d141ba4
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-07-26 11:43:05 +0700

    แก้ชื่อฟังก์ชันทดสอบ

--- internal/point/point_test.go
+++ internal/point/point_test.go
@@ -2,11 +2,11 @@ package point
 
 import "testing"
 
-func RecordPoint_By_MemberId_10010_Point_50_Should_Be_True(t *testing.T) {
+func RecordPoint_By_Member_Id_10010_Point_50_Should_Be_True(t *testing.T) {
 	expectedResult := true
 	action := Action{
-		UserReferral:   10010,
-		NewPoint: 50,
+		UserReferral: 10010,
+		NewPoint:     50,
 	}
 
 	actualResult := RecordPoint(action)
