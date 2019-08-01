816cdebd967f1335aa4aaeed48164355d708c134
uracha <uracha_p@kkumail.com>
2019-08-01 11:46:32 +0700

    ADD : ฟังก์ชันแลังฟังก์ชันทดสอบ Insert

--- database/repository/point.go
+++ database/repository/point.go
@@ -1,1 +1,18 @@
 package repository
+
+import (
+	"database/sql"
+	"multi-level-marketing-project/internal/members"
+)
+
+func Insert(db *sql.DB, newUserPoint members.NewUserPoint) bool {
+	statements, err := db.Prepare(`INSERT INTO point_records (member_id, point) VALUES (?,?)`)
+	if err != nil {
+		return false
+	}
+	_, err = statements.Exec(newUserPoint.UserRefferal, newUserPoint.NewPoint)
+	if err != nil {
+		return false
+	}
+	return true
+}
---
+++ database/repository/point_test.go
@@ -0,0 +1,33 @@
+package repository
+
+import (
+	"database/sql"
+	"multi-level-marketing-project/internal/members"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func DBConnection() (*sql.DB, error) {
+	db, err := sql.Open("mysql",
+		"root:root@tcp(10.10.99.37:3306)/mlm")
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func Test_Insert_By_MemberID_99999_Point_50_Should_be_True(t *testing.T) {
+	expectedResult := true
+	newUserPoint := members.NewUserPoint{
+		UserRefferal: 99999,
+		NewPoint:     50,
+	}
+	db, _ := DBConnection()
+	defer db.Close()
+	actualResult := Insert(db, newUserPoint)
+
+	if actualResult != expectedResult {
+		t.Errorf("Expect %v but get %v", expectedResult, actualResult)
+	}
+}
