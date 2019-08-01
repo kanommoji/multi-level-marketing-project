056465f3d76ce2567e2d27f1f80f7570031ef627
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-08-01 16:35:51 +0700

    ADD : ฟังก์ชัน AddPoint และฟังก์ชัน decodeRequest

--- cmd/handler/multilevel.go
+++ cmd/handler/multilevel.go
@@ -1,1 +1,44 @@
 package handler
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"multi-level-marketing-project/database/db"
+	"multi-level-marketing-project/internal/members"
+	"multi-level-marketing-project/internal/point"
+	"net/http"
+)
+
+func AddPoint(w http.ResponseWriter, r *http.Request) {
+	newUserPoint, err := decodeRequest(r)
+	if err != nil {
+		http.Error(w, "Error Decode request unsuccess", http.StatusBadRequest)
+		return
+	}
+	db, err := db.DBConnection()
+	if err != nil {
+		http.Error(w, "Error Database connection unsuccess", http.StatusInternalServerError)
+		return
+	}
+	if point.RecordPoint(db, newUserPoint) == false {
+		http.Error(w, "Error Record point unsuccess", http.StatusBadRequest)
+		return
+	} else {
+		members.VerifyLevel(newUserPoint.UserRefferal)
+	}
+}
+
+func decodeRequest(r *http.Request) (members.NewUserPoint, error) {
+	var newUserPoint members.NewUserPoint
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return newUserPoint, err
+	}
+	decode := json.NewDecoder(bytes.NewReader(body))
+	err = decode.Decode(&newUserPoint)
+	if err != nil {
+		return newUserPoint, err
+	}
+	return newUserPoint, nil
+}
