46757913fb0fc7482aef95501db91788ae013ae5
endkarn <end_karn@hotmail.com>
2019-08-01 17:50:40 +0700

    EDIT : แก้ไขการสร้าง connection ของการทดสอบ recordpoint

--- internal/point/point_test.go
+++ internal/point/point_test.go
@@ -1,21 +1,12 @@
 package point
 
 import (
-	"database/sql"
+	"multi-level-marketing-project/database/db"
 	"multi-level-marketing-project/models"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
 )
-
-func DBConnection() (*sql.DB, error) {
-	db, err := sql.Open("mysql",
-		"root:1640@tcp(127.0.0.1:3306)/mlm")
-	if err != nil {
-		return nil, err
-	}
-	return db, nil
-}
 
 func Test_RecordPoint_By_MemberID_99999_Point_50_Should_be_True(t *testing.T) {
 	expectedResult := true
@@ -23,9 +14,9 @@ func Test_RecordPoint_By_MemberID_99999_Point_50_Should_be_True(t *testing.T) {
 		UserRefferal: 99999,
 		NewPoint:     50,
 	}
-	db, _ := DBConnection()
-	defer db.Close()
-	actualResult := RecordPoint(db, newUserPoint)
+	connection, _ := db.DBConnectionLocal()
+	defer connection.Close()
+	actualResult := RecordPoint(connection, newUserPoint)
 
 	if actualResult != expectedResult {
 		t.Errorf("Expect %v but get %v", expectedResult, actualResult)
