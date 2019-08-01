f0210346b87e3c55ea2bb05fb896ed665aad6da4
uracha <uracha_p@kkumail.com>
2019-08-01 11:13:39 +0700

    ADD : ฟังก์ชัน DBConnection

--- database/db/database.go
+++ database/db/database.go
@@ -1,1 +1,15 @@
 package db
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func DBConnection() (*sql.DB, error) {
+	db, err := sql.Open("mysql",
+		"root:root@tcp(10.10.99.37:3306)/mlm")
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
