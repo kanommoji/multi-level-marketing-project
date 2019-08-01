9d9413a4a1ce6397407274f4b778938015d0371c
uracha <uracha_p@kkumail.com>
2019-08-01 10:47:26 +0700

    ADD : สร้าง struct Member และ TeamMember

--- internal/members/member.go
+++ internal/members/member.go
@@ -1,1 +1,18 @@
 package members
+
+type Member struct {
+	MemberID     int
+	MemberName   string
+	LeaderID     int
+	Level        int
+	MyPoint      int
+	MonthlyPoint int
+	TeamPoint    int
+	TeamMember   TeamMember
+}
+
+type TeamMember struct {
+	HigherPearl   int
+	HigherEmerald int
+	HigherRuby    int
+}
