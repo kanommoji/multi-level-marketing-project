c8e381db8cc9f428c4475830afa7f1f3aadbb635
endkarn <end_karn@hotmail.com>
2019-08-02 14:27:10 +0700

    ADD : เพิ่ม tag json ที่ member model

--- model/member.go
+++ model/member.go
@@ -1,13 +1,13 @@
 package model
 
 type Member struct {
-	MemberID     int
-	MemberName   string
-	LeaderID     int
-	Level        int
-	MyPoint      int
-	MonthlyPoint int
-	TeamPoint    int
+	MemberID     int    `json:"member_id"`
+	MemberName   string `json:"member_name"`
+	LeaderID     int    `json:"leader_id"`
+	Level        int    `json:"user_referral"`
+	MyPoint      int    `json:"my_point"`
+	MonthlyPoint int    `json:"monthly_point"`
+	TeamPoint    int    `json:"team_point"`
 	TeamMember   TeamMember
 }
 
