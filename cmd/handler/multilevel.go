5131f2d5114468ecc16b37dd487f94af58c95dc5
endkarn <end_karn@hotmail.com>
2019-08-02 15:40:55 +0700

    EDIT : แก้ไข function และ test ของ api GetMember

--- cmd/handler/multilevel.go
+++ cmd/handler/multilevel.go
@@ -2,18 +2,18 @@ package handler
 
 import (
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"multi-level-marketing-project/internal/member"
 	"net/http"
 	"strconv"
-
-	"github.com/gorilla/mux"
 )
 
 func GetMember(writer http.ResponseWriter, request *http.Request) {
 	vars := mux.Vars(request)
-	memberID, err := strconv.Atoi(vars["key"])
+	memberID, err := strconv.Atoi(vars["memberID"])
 	if err != nil {
 		http.Error(writer, err.Error(), http.StatusBadRequest)
+		return
 	}
 	member := member.FindMember(memberID)
 	encode := json.NewEncoder(writer)
