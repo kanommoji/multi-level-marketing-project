211f00cf4854ece104fe1919b4a362bcf7882dd4
endkarn <end_karn@hotmail.com>
2019-08-02 16:22:21 +0700

    EDIT : แก้ tag json ของข้อมูล member ที่ไม่ต้องการให้แสดงออกไป

--- model/member.go
+++ model/member.go
@@ -1,14 +1,14 @@
 package model
 
 type Member struct {
-	MemberID     int    `json:"member_id"`
-	MemberName   string `json:"member_name"`
-	LeaderID     int    `json:"leader_id"`
-	Level        int    `json:"level"`
-	MyPoint      int    `json:"my_point"`
-	MonthlyPoint int    `json:"monthly_point"`
-	TeamPoint    int    `json:"team_point"`
-	TeamMember   TeamMember
+	MemberID     int        `json:"member_id"`
+	MemberName   string     `json:"member_name"`
+	LeaderID     int        `json:"leader_id"`
+	Level        int        `json:"level"`
+	MyPoint      int        `json:"my_point"`
+	MonthlyPoint int        `json:"monthly_point"`
+	TeamPoint    int        `json:"team_point"`
+	TeamMember   TeamMember `json:"-"`
 }
 
 type TeamMember struct {
