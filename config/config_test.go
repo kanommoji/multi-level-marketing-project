d606f950223435934ad570b695ee8211cad7b212
uracha <uracha_p@kkumail.com>
2019-08-02 14:33:13 +0700

    EDIT : แก้ชื่อฟังก์ันทดสอบ GetURI

--- config/config_test.go
+++ config/config_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func Test_GetURI_Should_Be_(t *testing.T) {
+func Test_GetURI_Should_Be_Username_root_Password_root_Host_127_0_0_1_Port_3306_Database_mlm(t *testing.T) {
 	expectedResult := "root:root@tcp(127.0.0.1:3306)/mlm"
 	config := Config{
 		Username: "root",
