734fa0e8fd57cea7a41aa1f7e65cfd2da83e8187
uracha <uracha_p@kkumail.com>
2019-08-02 13:04:50 +0700

    ADD : สร้างฟังก์ชัน SetupConfig

--- config/config.go
+++ config/config.go
@@ -1,1 +1,26 @@
 package config
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"multi-level-marketing-project/model"
+
+	"gopkg.in/yaml.v2"
+)
+
+func SetupConfig() (model.Config, error) {
+	var config model.Config
+	environment := "development"
+	configFile, err := ioutil.ReadFile(fmt.Sprintf("config/%s.yml", environment))
+	if err != nil {
+		fmt.Printf("cannot read config file %s", err)
+		return config, err
+	}
+	err = yaml.NewDecoder(bytes.NewReader(configFile)).Decode(&config)
+	if err != nil {
+		fmt.Printf("cannot decode config file %s", err)
+		return config, err
+	}
+	return config, nil
+}
