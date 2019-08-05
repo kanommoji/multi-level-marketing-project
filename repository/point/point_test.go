76da11f8023bbe20e409393abbb8d495dd8640e7
uracha <uracha_p@kkumail.com>
2019-08-05 11:27:49 +0700

    ADD : ฟังก์ชันและฟังก์ชันทดสอบ Insert

--- repository/point/point_test.go
+++ repository/point/point_test.go
@@ -1,1 +1,29 @@
 package repository
+
+import (
+	"multi-level-marketing-project/config"
+	"multi-level-marketing-project/database"
+	"multi-level-marketing-project/model"
+	"testing"
+)
+
+func Test_Insert_By_UserReferral_99999_NewPoint_50_Should_Be_True(t *testing.T) {
+	expectedResult := true
+	config := config.Config{
+		Username: "root",
+		Password: "root",
+		Host:     "127.0.0.1",
+		Database: "mlm",
+		Port:     "3306",
+	}
+	database, _ := database.DBConnect(config.GetURI())
+	newUserPoint := model.NewUserPoint{
+		UserReferral: 99999,
+		NewPoint:     50,
+	}
+	actualResult := Insert(database, newUserPoint)
+
+	if expectedResult != actualResult {
+		t.Errorf("Expect %v but get %v", expectedResult, actualResult)
+	}
+}
