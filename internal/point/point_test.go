ab6d0908debdb2701c5401fa01fb5cb988442376
endkarn <end_karn@hotmail.com>
2019-08-06 10:20:45 +0700

    ADD : เพิ่ม test case ของ func recordpoint

--- internal/point/point_test.go
+++ internal/point/point_test.go
@@ -28,3 +28,25 @@ func Test_RecordPoint_By_MemberID_99999_Point_50_Should_Be_True(t *testing.T) {
 		t.Errorf("Expect %v but get %v", expectedResult, actualResult)
 	}
 }
+
+func Test_RecordPoint_By_MemberID_00000_Point_50_Should_Be_False(t *testing.T) {
+	expectedResult := false
+	config := config.Config{
+		Username: "root",
+		Password: "root",
+		Host:     "127.0.0.1",
+		Database: "mlm",
+		Port:     "3306",
+	}
+	database, _ := database.DBConnect(config.GetURI())
+	newUserPoint := model.NewUserPoint{
+		UserReferral: 00000,
+		NewPoint:     50,
+	}
+
+	actualResult := RecordPoint(database, newUserPoint)
+
+	if expectedResult != actualResult {
+		t.Errorf("Expect %v but get %v", expectedResult, actualResult)
+	}
+}
