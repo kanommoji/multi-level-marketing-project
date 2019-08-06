77b46ff4848b7670f8497beaa45a0313268b365c
endkarn <end_karn@hotmail.com>
2019-08-06 13:53:41 +0700

    ADD : เพิ่มกรณีทดสอบของ func TimeNow สำหรับรับค่าเวลาของระบบปัจจุบัน

--- config/config_test.go
+++ config/config_test.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"testing"
+	"time"
 )
 
 func Test_GetURI_Should_Be_Username_root_Password_root_Host_127_0_0_1_Port_3306_Database_mlm(t *testing.T) {
@@ -15,6 +16,16 @@ func Test_GetURI_Should_Be_Username_root_Password_root_Host_127_0_0_1_Port_3306_
 	}
 
 	actualResult := config.GetURI()
+
+	if expectedResult != actualResult {
+		t.Errorf("Expect %v but get %v", expectedResult, actualResult)
+	}
+}
+
+func Test_TimeNow_Shout_Be_Date_Year_2019_Month_7_Day_2(t *testing.T) {
+	expectedResult := time.Date(2019, 7, 2, 0, 0, 0, 0, time.UTC)
+
+	actualResult := TimeNow()
 
 	if expectedResult != actualResult {
 		t.Errorf("Expect %v but get %v", expectedResult, actualResult)
